2db79eedd4a96666a5b520e9057e8f551957600c
theanarkh <2923878201@qq.com>
2019-11-07 15:41:19 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,19 +1,20 @@
 const { assign } = require('./');
 
 {
-	const source = {a:{d:1, b:{c:[1,2,3]}}};
-	const result = assign(source, 'a.b', {c: []});
-	console.log(result);
-
-}
-{
-	const source = {a:{d:1, b:{c:[1,2,3]}}};
-	const result = assign(source, 'a.b', {d: []});
-	console.log(result);
+	assign({a: {b:{c: [1,2,3]}}}, [
+	    {path: 'a', action: 'merge', data: {test: 1}}, 
+	    {path: 'a', action: 'merge', data: {test1: 2}}, 
+	    {path: 'a.b', action: 'merge', data: {test: 1}}
+	])
+	console.log(JSON.stringify(result))
 }
 
 {
-	const source = [{a:1},{a:2}];
-	const result = assign(source, '[1]', {a:3});
-	console.log(result);
+	assign([{a:1}, {a: 2}], [
+    	{path: '[1]', action: 'merge', data: {a: 3}}, 
+	])
 }
+
+assign([{a:1}, {a: 2}], [
+    {path: '[1].a', action: 'merge', data: 3}, 
+])
